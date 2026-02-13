fcaad5abef86f102b8982a6fe738bb0a805dd620
Yam Guragain <yamguragain30@gmail.com>
2026-02-13 18:12:36 +0545

    try again

--- modules/auth/auth.routes.js
+++ modules/auth/auth.routes.js
@@ -38,7 +38,7 @@ router.get(
     res.cookie("token", token, {
       httpOnly: true,
       secure: true,
-      sameSite: "none",
+      sameSite: "lax",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
@@ -114,7 +114,7 @@ router.post("/login", async (req, res) => {
   res.cookie("token", token, {
     httpOnly: true,
     secure: true,
-    sameSite: "none",
+    sameSite: "lax",
     maxAge: 7 * 24 * 60 * 60 * 1000,
   });
 
